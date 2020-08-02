2c35b4d4817bea6df44290a5406097127717a65f
John O'Reilly <oreillyj@gmail.com>
2020-08-02 09:16:51 +0100

    SwiftUI updates

--- ios/BikeShare/BikeShare/ContentView.swift
+++ ios/BikeShare/BikeShare/ContentView.swift
@@ -28,6 +28,7 @@ struct ContentView : View {
 struct StationListView: View {
     @ObservedObject var cityBikesViewModel : CityBikesViewModel
     var network: String
+    let timer = Timer.publish(every: 30, on: .main, in: .common).autoconnect()
  
     var body: some View {
         NavigationView {
@@ -35,6 +36,9 @@ struct StationListView: View {
                 StationView(station: station)
             }
             .navigationBarTitle(Text("Bike Share"))
+            .onReceive(timer) { _ in
+                self.cityBikesViewModel.fetch(network: self.network)
+            }
             .onAppear(perform: {
                 self.cityBikesViewModel.fetch(network: self.network)
             })
@@ -44,18 +48,26 @@ struct StationListView: View {
 
 struct StationView : View {
     var station: Station
-    
+
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(station.name).font(.headline)
-            //Text(station.freeBikes ?? "").font(.subheadline)
-            HStack {
-                Text("Free:").font(.subheadline).frame(width: 80, alignment: .leading)
-                Text("\(station.freeBikes())").font(.subheadline)
-            }
-            HStack {
-                Text("Slots:").font(.subheadline).frame(width: 80, alignment: .leading)
-                Text("\(station.slots())").font(.subheadline)
+        HStack {
+            Image("ic_bike").resizable()
+                .renderingMode(.template)
+                .foregroundColor(station.freeBikes() < 5 ? .orange : .green)
+                .frame(width: 32.0, height: 32.0)
+            
+            Spacer().frame(width: 16)
+            
+            VStack(alignment: .leading) {
+                Text(station.name).font(.headline)
+                HStack {
+                    Text("Free:").font(.subheadline).frame(width: 80, alignment: .leading)
+                    Text("\(station.freeBikes())").font(.subheadline)
+                }
+                HStack {
+                    Text("Slots:").font(.subheadline).frame(width: 80, alignment: .leading)
+                    Text("\(station.slots())").font(.subheadline)
+                }
             }
         }
     }
