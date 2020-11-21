01293d68f70dcbdace811f3877607cd79ed1210f
John O'Reilly <oreillyj@gmail.com>
2020-11-21 18:23:19 +0000

    desktop compose: show map with bike stations

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Jetpack Compose and SwiftUI based Kotlin Multiplatform sample project (based on [CityBikes API](http://api.citybik.es/v2/)).
 
 Running on
-* iOS
-* macOS
-* Android
+* iOS (SwiftUI)
+* macOS (SwiftUI)
+* Android (Jetpack Compose)
+* Desktop (Jetpack Compose)
 
 ![BikeShare Screenshot](/art/screenshot1.png?raw=true )
 ![BikeShare Screenshot](/art/screenshot2.png?raw=true )
@@ -17,6 +18,11 @@ When opening iOS and macOS projects remember to open `.xcworkspace` file (and no
 
 ### macOS client
 The macOS client uses functionality only available in SwiftUI 2 and as a result requires use of XCode 12.2 beta.
+
+### Jetpack Compose for Desktop client
+
+This client is available in `compose-desktop` module.  Note that you currently need to use EAP version of kotlin
+plugin and also use appropriate JVM when running (works for example with Java 11)
 
 
 Note that this makes use of https://github.com/Shusshu/android-flags for flag images shown.
--- compose-desktop/src/main/kotlin/main.kt
+++ compose-desktop/src/main/kotlin/main.kt
@@ -1,54 +1,81 @@
-
 import androidx.compose.desktop.AppManager
-import androidx.compose.desktop.AppWindow
-import androidx.compose.desktop.Window
-import androidx.compose.foundation.Text
+import androidx.compose.desktop.ComposePanel
+import androidx.compose.desktop.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumnFor
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.surrus.common.remote.CityBikesApi
 import com.surrus.common.remote.Network
-import kotlinx.coroutines.flow.map
-import java.util.*
+import com.surrus.common.remote.Station
 import org.jxmapviewer.JXMapViewer
-import javax.swing.JFrame
-import org.jxmapviewer.viewer.GeoPosition
-
-import org.jxmapviewer.viewer.DefaultTileFactory
-
 import org.jxmapviewer.OSMTileFactoryInfo
-
-import org.jxmapviewer.viewer.TileFactoryInfo
-import java.awt.BorderLayout
-import javax.swing.JButton
-import javax.swing.JLabel
-import javax.swing.JPanel
-import javax.swing.SwingUtilities.invokeLater
+import org.jxmapviewer.viewer.*
+import java.awt.GridLayout
+import java.util.*
+import javax.swing.JFrame
+import javax.swing.WindowConstants
 
 
 data class Country(val code: String, val displayName: String)
 
+fun main() {
+    AppManager.setEvents(onAppStart = null, onAppExit = null, onWindowsEmpty = null)
+    SwingComposeWindow()
+}
 
 
-fun main() = Window {
+var map: JXMapViewer? = null
+
+
+fun SwingComposeWindow() {
+    val window = JFrame()
+    window.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+    window.title = "BikeShare"
+
+    map = createMap()
+
+    val composePanel = ComposePanel()
+    window.layout = GridLayout(1, 2)
+    window.add(composePanel)
+    window.add(map)
+
+    composePanel.setContent {
+        BikeShareView()
+    }
+
+    window.setSize(800, 600)
+    window.isVisible = true
+}
+
+fun createMap() : JXMapViewer {
+    val mapViewer = JXMapViewer()
+
+    val info: TileFactoryInfo = OSMTileFactoryInfo()
+    val tileFactory = DefaultTileFactory(info)
+    mapViewer.tileFactory = tileFactory
+
+    mapViewer.zoom = 5
+    return mapViewer
+}
+
+@Composable
+fun BikeShareView()  {
     val cityBikesApi = CityBikesApi()
+    var networkId by remember { mutableStateOf("") }
     var networkList by remember { mutableStateOf(emptyList<Network>()) }
-
-    var groupedNetworkList: Map<Country,List<Network>> by remember { mutableStateOf(emptyMap()) }
+    var stationList by remember { mutableStateOf(emptyList<Station>()) }
+    var groupedNetworkList: Map<Country, List<Network>> by remember { mutableStateOf(emptyMap()) }
     var countryList by remember { mutableStateOf(emptyList<Country>()) }
 
 
-    launchInComposition {
-
+    LaunchedEffect(true) {
         val allNetworks = cityBikesApi.fetchNetworkList().networks
         val groupedNetworkListByCountryCode = allNetworks.groupBy { it.location.country }
         groupedNetworkList = groupedNetworkListByCountryCode.mapKeys {
@@ -62,49 +89,56 @@ fun main() = Window {
         println(countryList)
     }
 
+    LaunchedEffect(networkId) {
+        if (networkId.isNotEmpty()) {
+            stationList = cityBikesApi.fetchBikeShareInfo(networkId).network.stations
 
-    val mapViewer = JXMapViewer()
+            val wpp = WaypointPainter<Waypoint>()
+            val wpSet = mutableSetOf<Waypoint>()
+            stationList.forEach {
+                val wp = Waypoint() {
+                    GeoPosition(it.latitude, it.longitude)
+                }
+                wpSet.add(wp)
+            }
 
-    // Create a TileFactoryInfo for OpenStreetMap
-    val info: TileFactoryInfo = OSMTileFactoryInfo()
-    val tileFactory = DefaultTileFactory(info)
-    mapViewer.tileFactory = tileFactory
-
-    // Set the focus
-    val frankfurt = GeoPosition(50.11, 8.68)
-
-    mapViewer.zoom = 7
-    mapViewer.addressLocation = frankfurt
-
-
-    val frame = JFrame("JXMapviewer2 Example 1")
-    frame.contentPane.add(mapViewer)
-    frame.setSize(800, 600)
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    frame.isVisible = true
+            wpp.setWaypoints(wpSet)
+            map?.overlayPainter = wpp
+        }
+    }
 
     MaterialTheme {
         Row {
-            LazyColumnFor(items = countryList, itemContent = { country ->
-                Row(Modifier.clickable(onClick = {
-                        networkList = groupedNetworkList[country]!!
-                    })
-                    + Modifier.padding(start = 16.dp, top = 4.dp, end = 16.dp, bottom = 4.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
+            Box(Modifier.width(200.dp).fillMaxHeight()) {
+                LazyColumnFor(items = countryList, itemContent = { country ->
+                    Row(
+                        Modifier.clickable(onClick = {
+                            networkList = groupedNetworkList[country]!!
+                        }).padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(country.displayName)
+                    }
+                })
+            }
 
-                    Text(country.displayName)
-                }
-            })
-            LazyColumnFor(items = networkList, itemContent = { network ->
-                Row(Modifier.clickable(onClick = {
-                })  + Modifier.padding(start = 16.dp, top = 4.dp, end = 16.dp, bottom = 4.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text("${network.name} (${network.location.city})")
-                }
+            Spacer(modifier = Modifier.preferredWidth(1.dp).fillMaxHeight()
+                    .background(color = MaterialTheme.colors.onSurface.copy(0.25f)))
 
-            })
+            Box {
+                LazyColumnFor(items = networkList, itemContent = { network ->
+                    Row(
+                        Modifier.clickable(onClick = {
+                            networkId = network.id
+                            val city = GeoPosition(network.location.latitude, network.location.longitude)
+                            map?.addressLocation = city
+                        }).padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("${network.name} (${network.location.city})")
+                    }
+                })
+            }
 
         }
     }
