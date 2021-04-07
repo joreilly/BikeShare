a5ab23ffa624ba79e545c5827f09bb691b2edc8c
John O'Reilly <oreillyj@gmail.com>
2021-04-07 19:55:23 +0100

    nav compose alpha10

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val ktx = "1.0.1"
     const val lifecycle = "2.2.0-alpha01"
     const val compose = "1.0.0-beta04"
-    const val nav_compose = "1.0.0-alpha09"
+    const val nav_compose = "1.0.0-alpha10"
     const val accompanist = "0.7.0"
     const val slf4j = "1.7.30"
 
