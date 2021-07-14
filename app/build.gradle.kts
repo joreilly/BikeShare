8054702c918968015642f2bba8a969c1879b4184
John O'Reilly <oreillyj@gmail.com>
2021-07-14 21:53:07 +0100

    Jetpack Compose rc2 + related dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,9 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.activity:activity-compose:1.3.0-rc01")
+    implementation("androidx.activity:activity-compose:1.3.0-rc02")
 
     implementation(Compose.ui)
     implementation(Compose.uiGraphics)
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,9 +7,9 @@ object Versions {
     const val koin = "3.1.1"
     const val ktx = "1.0.1"
     const val lifecycle = "2.2.0-alpha01"
-    const val compose = "1.0.0-rc01"
+    const val compose = "1.0.0-rc02"
     const val nav_compose = "2.4.0-alpha04"
-    const val accompanist = "0.13.0"
+    const val accompanist = "0.14.0"
     const val slf4j = "1.7.30"
 
     const val kermit = "0.1.8"
