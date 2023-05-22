f4d477aa891c5b9ce3acbcf750652f8b94998ade
John O'Reilly <oreillyj@gmail.com>
2023-05-22 19:25:12 +0100

    dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("com.android.tools.build:gradle:8.0.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.8.20-1.0.11")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.8.21-1.0.11")
         classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutinesVersion}")
         classpath("io.realm.kotlin:gradle-plugin:${Versions.realm}")
     }
@@ -19,8 +19,16 @@ allprojects {
         mavenCentral()
         maven(url = "https://maven.pkg.jetbrains.space/public/p/ktor/eap")
         maven(url = "https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
-        //maven(url = "https://androidx.dev/snapshots/builds/7909927/artifacts/repository")
         maven("https://androidx.dev/storage/compose-compiler/repository")
     }
 }
 
+allprojects {
+    configurations.all {
+        resolutionStrategy.dependencySubstitution {
+            substitute(module("org.jetbrains.compose.compiler:compiler")).apply {
+                using(module("androidx.compose.compiler:compiler:${Versions.composeCompiler}"))
+            }
+        }
+    }
+}
